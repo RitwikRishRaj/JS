e175c2178528f0f7558662aaca53153d9077a3c7
RitwikRishRaj <devilrqop@gmail.com>
2024-10-08 20:28:45 +0530

    datatypes

---
+++ basics/2datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+/* alert(3+3) will throw error coz am using node JS, but in browser console it will show 6
+it's available but cant use now*/
+console.log(3+3); 
+console.log("Ritwik");
+
+let name = "Ritwik"
+let age = 17
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not assigned value
+//symbol (basically more in react) => unique
+
+
+
+//object 
+
+console.log(typeof null);
+// null is object type,  but undefined type is undefined
+
