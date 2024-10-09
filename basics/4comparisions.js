c62c2761fa1fe99523878b50b6928cbf6fee1850
RitwikRishRaj <devilrqop@gmail.com>
2024-10-09 20:29:29 +0530

    comparision summary types of memory Stack&Heap

--- basics/4comparisions.js
+++ basics/4comparisions.js
@@ -20,7 +20,7 @@ console.log("2"=== 2);
 
 
 //************summary*************** 
-
+//types of memory
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
@@ -58,3 +58,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//************************************* 
+
+//Types of memory
+//stack (primitive), heap (Non-primitive)
+
+let myYtName = "ritwik.com"
+let anotherName = myYtName
+anotherName = "rishuraj"
+console.log(myYtName);
+console.log(anotherName);
+
+
+
+let user = {
+    email: "user@google.com",
+    upi: "387964@yhsbi"
+
+}
+
+let user1 = user
+user1.email = "ritwik@gmail.com"
+
+console.log(user.email);
+console.log(user1.email);
+
